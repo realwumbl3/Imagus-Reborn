b23dfd9ce280299f958329bd2b1e90f8f34609d6
hababr <hhababr@gmail.com>
2026-04-04 22:13:23 +0300

    update build script

--- build.sh
+++ build.sh
@@ -1,33 +1,29 @@
 #!/bin/bash
 
-die() {
-    echo "Error!"
-    waitkey
-    exit 1
-}
+# source directory
+src="src"
 
-waitkey() {
-    read -n 1 -s -r -p "Press any key to continue"
-}
+die() { echo -e "\nError!" && waitkey && exit 1; }
+waitkey() { read -n 1 -s -r -p "Press any key to continue"; }
 
-echo "Copying 'src' to 'chrome'..."
-mkdir -p ./build/chrome
-rsync -r -m --delete ./src/ ./build/chrome/ || die
+echo "Cleaning the build directory..."
+mkdir -p ./build || die
+rm -rf ./build/* || die
 
+echo -e "\nCopying '$src' to 'chrome'..."
+cp -r ./$src/. ./build/chrome/ || die
 echo "Zipping the Chrome build..."
 cd ./build/chrome || die
-rm -f manifest_firefox.json
-zip -9 -q -r ImagusReborn_Chrome_v`sed -n -E 's/^.*"version":\s*"(\S*)".*/\1/p' manifest.json`.zip *
+rm -f manifest_firefox.json || die
+zip -9 -q -r ../ImagusReborn_Chrome_v`sed -n -E 's/^.*"version":\s*"(\S*)".*/\1/p' manifest.json`.zip *
 
 cd ../..
-echo "Copying 'src' to 'firefox'..."
-mkdir -p ./build/firefox
-rsync -r -m --delete ./src/ ./build/firefox/ || die
-
+echo -e "\nCopying '$src' to 'firefox'..."
+cp -r ./$src/. ./build/firefox/ || die
 echo "Zipping the Firefox build..."
 cd ./build/firefox || die
 mv -f manifest_firefox.json manifest.json
-zip -9 -q -r ImagusReborn_Firefox_v`sed -n -E 's/^.*"version":\s*"(\S*)".*/\1/p' manifest.json`.zip *
+zip -9 -q -r ../ImagusReborn_Firefox_v`sed -n -E 's/^.*"version":\s*"(\S*)".*/\1/p' manifest.json`.zip *
 
-echo "Done!"
+echo -e "\nDone!"
 waitkey
